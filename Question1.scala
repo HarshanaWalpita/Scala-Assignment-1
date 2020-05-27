17e9d2f1157426567f1d8aa7d4ecbe04683d013b
HarshanaC <harshanawalpita@gmail.com>
2020-05-27 22:23:54 +0530

    first commit

--- Question1.scala
+++ Question1.scala
@@ -1,6 +1,2 @@
-def normal_working_salary(x:Int)=x*150
-def ot_working_salary(x:Int)=x*75
-def full_salary(x:Int,y:Int)=normal_working_salary(x)+ot_working_salary(y)
-def tax(full_salary:Int)=full_salary*.1
-def final_salary(x:Int,y:Int)=full_salary(x,y)-tax(full_salary(x,y))
-println(final_salary(40,20))
+def convert(x:Double):Double=x*1.8000+32.00
+println(convert(35))
